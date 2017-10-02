bb33cd569d392b1fc3c51b3d89993a0fea4e10ab
York <aarghgames@gmail.com>
2017-10-02 22:19:14 +0100

    Added second page example
    added foo-bar

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/foo', function(req, res) {
+  res.send('bar!');
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
